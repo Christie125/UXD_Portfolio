31f086a42c259e061aedc67f850124e309ebab4d
Christie125 <24125@gdc.school.nz>
2026-03-11 19:23:54 +1300

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/pages/home-page/home-page';
+import { AboutPage } from './components/pages/about-page/about-page';
+import { ProjectsPage } from './components/pages/projects-page/projects-page';
+import { ContactPage } from './components/pages/contact-page/contact-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage,
+        title: 'Christie B.'
+    },
+    {
+        path: 'about',
+        component: AboutPage,
+        title: 'About - Christie B.'
+    },
+    {
+        path: 'projects',
+        component: ProjectsPage,
+        title: 'Projects - Christie B.'
+    },
+    {
+        path: 'contact',
+        component: ContactPage,
+        title: 'Contact - Christie B.'
+    }
+];
